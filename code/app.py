b83b932b61c74467c93e2845f6fc55398d2a6aa5
LocSon <son.hua@cmon.io>
2024-07-06 10:31:35 +0700

    chore: Add Dockerfile and docker-compose for Detectron2 integration

--- code/app.py
+++ code/app.py
@@ -1,7 +1,7 @@
 # Serve model as a flask application
 from ultralytics import YOLO
 import numpy as np
-from flask import Flask, request
+from flask import Flask, request, send_file
 
 model = None
 app = Flask(__name__)
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 def load_model():
     global model
-    model = YOLO('./model/model_final.pth')
+    # model = YOLO('./model/model_final.pth')
+    model = YOLO("yolov8m-seg.pt")
 
 
 @app.route('/')
@@ -24,10 +25,11 @@ def main():
             # print("request",request.form)
             image=request.files['image']
             image_name=image.filename
-            if '.jpg' in image_name:
+            if '.jpg' in image_name or '.png' in image_name or '.jpeg' in image_name:
                 image.save(image_name)
                 # load image to model and get prediction
-                results=model(image_name)
+                results=model(image_name, save=True)
+                # results = model('https://ultralytics.com/images/bus.jpg', save=True)  # predict on an image
                 for result in results:
                     boxes = result.boxes  # Boxes object for bounding box outputs
                     masks = result.masks  # Masks object for segmentation masks outputs
@@ -36,12 +38,18 @@ def main():
                     obb = result.obb  # Oriented boxes object for OBB outputs
                     result.save(filename='result.jpg')  # save to disk
                 # return prediction
-                return {"response"}
+                return send_file("result.jpg",mimetype='image/jpg')
             else:
                 return {"error":"select you image file"}
     except Exception as e:
         return {"error":str(e)}
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000') # Replace with your Next.js app origin
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers.add('Access-Control-Allow-Methods', 'POST')
+    return response
 
 
 if __name__ == '__main__':
